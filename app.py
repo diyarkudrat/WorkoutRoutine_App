d0ce9d7a95e2b9dded54bfa72d15e9dbeefcda20
Diyar Kudrat <dkudrat@gmail.com>
2019-10-15 13:25:41 -0700

    added MongoDB

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.User
+users = db.users
 
 app = Flask(__name__)
 
@@ -6,9 +11,14 @@ app = Flask(__name__)
 def index():
     return render_template('homepage.html', msg = 'Welcome!')
 
-@app.route('/workout/create_account')
-def create_account():
+@app.route('/workout/create_user')
+def create_user():
     return render_template('create_account.html')
+
+@app.route('/workout/new_user', methods = ['POST'])
+def submit_user():
+    print(request.form.to_dict())
+    return redirect(url_for('homepage.html'))
 
 
 if __name__ == '__main__':
