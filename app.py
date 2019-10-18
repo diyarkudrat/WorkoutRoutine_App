01a807dded169e2ccb673fa0b41daeb78ddd91a6
Diyar Kudrat <dkudrat@gmail.com>
2019-10-18 10:18:35 -0700

    attempt to fix heroku issue

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def back():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000), retryWrites=False)
